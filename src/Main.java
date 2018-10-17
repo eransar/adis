920035db99f720771118d223f9e359fdeed75682
idanf <idanforu@gmail.com>
2018-10-17 17:39:00 +0300

    Delete sample.fxml create view package - view and fxml

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.Database;
+import view.View;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/View.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         Database d = new Database();
